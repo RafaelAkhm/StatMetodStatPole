47f65b808b3c938e37363b9780d5bf44ed07875e
RafaelAkhm <98662815+RafaelAkhm@users.noreply.github.com>
2022-02-26 14:43:27 +0400

    Homework 17 conditions for the elimination game
     Оформление игры в виде отдельного класса, который бы в статическом поле хранил состояние светофора (включён ли красный свет или зелёный свет) и имел бы статический метод, принимающий параметром скорость и возвращающий ответ на вопрос, выбыл ли игрок с этой скоростью при текущем свете светофора.

---
+++ Utils.java
@@ -0,0 +1,18 @@
+package my.favorite.code;
+
+public class Utils {
+    public static boolean isReedLightLight;
+
+    public static void printDelim1(String title) {
+        System.out.println("Игрок 1 - выбыл ");
+    }
+
+    public static void printDelim2(String title) {
+        System.out.println("Игрок 2 - выбыл ");
+    }
+
+    public static void printDelim3(String title) {
+        System.out.println("Игрок 3 - выбыл ");
+    }
+}
+
